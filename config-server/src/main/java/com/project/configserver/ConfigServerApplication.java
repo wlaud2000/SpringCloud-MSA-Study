5feb6688102e67e5d1ccff3b3731415eeb758499
Jimyeong Kim <kjm021221@icloud.com>
2025-02-23 12:43:01 +0900

    ✨feat: config-server 기본 설정(Security 포함)

--- config-server/src/main/java/com/project/configserver/ConfigServerApplication.java
+++ config-server/src/main/java/com/project/configserver/ConfigServerApplication.java
@@ -2,8 +2,10 @@ package com.project.configserver;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.config.server.EnableConfigServer;
 
 @SpringBootApplication
+@EnableConfigServer // Config 서버로서의 기능 수행
 public class ConfigServerApplication {
 
     public static void main(String[] args) {
