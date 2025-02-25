2712ded6cb0119d20744678b7d6d97e232cbf48e
Jimyeong Kim <kjm021221@icloud.com>
2025-02-25 16:38:29 +0900

    ✨feat: 게이트웨이서버에 Actuator 관련 Security 설정 추가

---
+++ apiGateway/src/main/java/com/project/apigateway/config/SecurityConfig.java
@@ -0,0 +1,36 @@
+package com.project.apigateway.config;
+
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.reactive.EnableWebFluxSecurity;
+import org.springframework.security.config.web.server.ServerHttpSecurity;
+import org.springframework.security.core.userdetails.MapReactiveUserDetailsService;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.server.SecurityWebFilterChain;
+
+@Configuration
+@EnableWebFluxSecurity // API Gateway는 WebFlux 기반
+public class SecurityConfig {
+
+    @Bean
+    public SecurityWebFilterChain securityWebFilterChain(ServerHttpSecurity http) {
+        return http
+                .csrf(ServerHttpSecurity.CsrfSpec::disable)
+                .authorizeExchange(exchanges -> exchanges
+                        // Actuator 엔드포인트에는 인증 필요
+                        .pathMatchers("/actuator/**").hasRole("ADMIN")
+                        // 그 외 경로는 모두 허용
+                        .anyExchange().permitAll()
+                )
+                .httpBasic(Customizer.withDefaults())
+                .build();
+    }
+}
